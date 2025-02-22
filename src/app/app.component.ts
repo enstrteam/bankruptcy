028ec0afa8afb11144d96883a6e8423ddd3610ff
enstrteam <enstrteam@gmail.com>
2025-02-23 00:30:33 +0300

    Adding favicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit {
       { property: 'og:description', content: 'Банкротство физических лиц в Уфе. Узнайте, как оформить банкротство с рассрочкой, стоимость услуги, отзывы клиентов и профессиональные услуги юристов по банкротству в Уфе.' },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: 'https://ufabankruptcy.ru' }, // Актуальный URL
-      { property: 'og:image', content: 'https://ufabankruptcy.ru/preview-image.webp' }, // Путь к изображению для соцсетей
+      { property: 'og:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, // Путь к изображению для соцсетей
       { property: 'og:image:width', content: '1200' },
       { property: 'og:image:height', content: '630' },
       { property: 'og:site_name', content: 'Банкротство Уфа' },
@@ -51,7 +51,7 @@ export class AppComponent implements OnInit {
       { name: 'twitter:card', content: 'summary_large_image' }, // Используем карточку с большим изображением
       { name: 'twitter:title', content: 'Банкротство физических лиц в Уфе с рассрочкой | Отзывы, Цена и Услуги Юристов' },
       { name: 'twitter:description', content: 'Банкротство физических лиц в Уфе. Узнайте, как оформить банкротство с рассрочкой, стоимость услуги, отзывы клиентов и профессиональные услуги юристов по банкротству в Уфе.' },
-      { name: 'twitter:image', content: 'https://ufabankruptcy.ru/preview-image.webp' }, // Укажи путь к изображению
+      { name: 'twitter:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, // Укажи путь к изображению
       { name: 'twitter:image:alt', content: 'Банкротство физических лиц в Уфе – избавление от долгов с возможностью рассрочки' },
     ]);
   }
