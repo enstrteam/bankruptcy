4f8f8494f07277d3a62e7e058cd4a2ad8d8e82bb
enstrteam <enstrteam@gmail.com>
2025-02-23 00:48:11 +0300

    Fixing base url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,13 +34,12 @@ export class AppComponent implements OnInit {
       content: 'банкротство уфа, банкротство физических лиц уфа, банкротство с рассрочкой, цена банкротства уфа, отзывы банкротство, юристы по банкротству, банкротство долгов, адвокат по банкротству физических лиц'
     });
 
-    // Добавляем другие мета-теги для соцсетей и SEO
     this.metaService.addTags([
       { property: 'og:title', content: 'Банкротство физических лиц в Уфе с рассрочкой | Отзывы, Цена и Услуги Юристов' },
       { property: 'og:description', content: 'Банкротство физических лиц в Уфе. Узнайте, как оформить банкротство с рассрочкой, стоимость услуги, отзывы клиентов и профессиональные услуги юристов по банкротству в Уфе.' },
       { property: 'og:type', content: 'website' },
-      { property: 'og:url', content: 'https://ufabankruptcy.ru' }, // Актуальный URL
-      { property: 'og:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, // Путь к изображению для соцсетей
+      { property: 'og:url', content: 'https://ufabankruptcy.ru' }, 
+      { property: 'og:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, 
       { property: 'og:image:width', content: '1200' },
       { property: 'og:image:height', content: '630' },
       { property: 'og:site_name', content: 'Банкротство Уфа' },
@@ -48,10 +47,10 @@ export class AppComponent implements OnInit {
     ]);
 
     this.metaService.addTags([
-      { name: 'twitter:card', content: 'summary_large_image' }, // Используем карточку с большим изображением
+      { name: 'twitter:card', content: 'summary_large_image' }, 
       { name: 'twitter:title', content: 'Банкротство физических лиц в Уфе с рассрочкой | Отзывы, Цена и Услуги Юристов' },
       { name: 'twitter:description', content: 'Банкротство физических лиц в Уфе. Узнайте, как оформить банкротство с рассрочкой, стоимость услуги, отзывы клиентов и профессиональные услуги юристов по банкротству в Уфе.' },
-      { name: 'twitter:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, // Укажи путь к изображению
+      { name: 'twitter:image', content: 'https://ufabankruptcy.ru/assets/images/preview-image.webp' }, 
       { name: 'twitter:image:alt', content: 'Банкротство физических лиц в Уфе – избавление от долгов с возможностью рассрочки' },
     ]);
   }
